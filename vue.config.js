75f231a281b7a5a46eba3b0f16fb9564f32f1c84
Matt Coleman <matt.coleman@protonmail.com>
2021-04-13 11:59:15 +0100

    Added meta title and description

---
+++ vue.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  pages: {
+    index: {
+      entry: 'src/main.js',
+      title: 'T&F Sports Apparel',
+      description: 'T&F Sports Apparel designs and produces sports merchandise'
+    }
+  }
+}
